408477c173e2ad4a83f49a04a6069b1fe5de81ae
Michal Krawczyk <michalkrawczyk1@gmail.com>
2022-08-25 17:20:14 +0100

    write all variables to manipulate temperature converter

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// Temperature converter
+
+// Variables needed to manipulate temperature converter 
+const tempConvInput = document.querySelector("#converter-temp");
+const fromTemp = document.querySelector(".conv-from-temp");
+const toTemp = document.querySelector(".conv-to-temp");
+const resultTemp = document.querySelector(".res-temp");
+const convTempBtn = document.querySelector('.btn-temp-conv')
+const changeTempBtn = document.querySelector('.temp-change')
+const resetTempBtn = document.querySelector('.reset-temp')
+
+// Temperature converter functions
+
+
+
