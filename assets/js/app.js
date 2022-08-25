a8ccbb6fbc3838fe64c34c168657430723bf9c38
Michal Krawczyk <michalkrawczyk1@gmail.com>
2022-08-25 17:39:18 +0100

    reset and swap side functions added with listeners

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,5 +11,37 @@ const resetTempBtn = document.querySelector('.reset-temp')
 
 // Temperature converter functions
 
+function swapTempValues(){
+    if(fromTemp.textContent === '°C'){
+        fromTemp.textContent = '°F'
+        toTemp.textContent = '°C'
+    }else{
+        fromTemp.textContent = '°C'
+        toTemp.textContent = '°F'
+    }
+}
+
+function convertTemp(){
+    if(fromTemp.textContent === '°C'){
+        resultTemp.textContent = 
+    }
+}
+
+function fahrToCel(){
+    const fahr = tempConvInput.value * 1.8 + 32
+
+}
+
+
+function restetValues(){
+    fromTemp.textContent = '°C'
+    toTemp.textContent = '°F'
+    tempConvInput.value = 'Type number...'
+}
+
+// Event Listeners
+convTempBtn.addEventListener('click' convertTemp)
+changeTempBtn.addEventListener('click',swapTempValues)
+resetTempBtn.addEventListener('click', restetValues)
 
 
