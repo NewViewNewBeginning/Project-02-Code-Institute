26758f9110d32e06ac6548d766918aff36f79b9c
Michal Krawczyk <michalkrawczyk1@gmail.com>
2022-08-30 16:24:05 +0100

    create 1 function to swap conversion side on each converter

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 // Update and display current year in footer
 
-document.querySelector('.date').appendChild(document.createTextNode(new Date().getFullYear()))
+document
+	.querySelector(".date")
+	.appendChild(document.createTextNode(new Date().getFullYear()));
 
 // Temperature converter
 
@@ -10,24 +12,10 @@ const fromTemp = document.querySelector(".conv-from-temp");
 const toTemp = document.querySelector(".conv-to-temp");
 const resultTemp = document.querySelector(".res-temp");
 const convTempBtn = document.querySelector(".btn-temp-conv");
-const changeTempBtn = document.querySelector(".temp-change");
-const resetTempBtn = document.querySelector(".reset-temp");
+
 const tempImg = document.querySelector(".temp-img");
 
 // Temperature converter functions
-
-function swapTempValues() {
-	tempConvInput.value = "";
-	resultTemp.style.color = "var(--font-color)";
-	resultTemp.textContent = "Result:";
-	if (fromTemp.textContent === "°C") {
-		fromTemp.textContent = "°F";
-		toTemp.textContent = "°C";
-	} else {
-		fromTemp.textContent = "°C";
-		toTemp.textContent = "°F";
-	}
-}
 
 function convertTemp() {
 	if (tempConvInput.value !== "") {
@@ -70,64 +58,98 @@ function fahrToCel() {
 		tempImg.src = "./assets/img/winter-cold.jpg";
 	}
 }
-// function restetValues() {
-//     fromTemp.textContent = "°C";
-//     toTemp.textContent = "°F";
-//     resultTemp.textContent = "Result:";
-//     resultTemp.style.color = "var(--font-color)";
-//     tempConvInput.value = "";
-// }
 
 // Meter Foot conversion
 
-function lengthConverter(){
+function lengthConverter() {
 	// let meter = footNumberInput / 0.3048.toFixed(2)
-    //  let foot = meterNumber * 3.28084 .toFixed(2)
+	//  let foot = meterNumber * 3.28084 .toFixed(2)
 }
 
 // Speed conversion
 
-function speedConverter(){
-    // let miles = km × 0.621371
-    // let km = miles × 1.609344
+function speedConverter() {
+	// let miles = km × 0.621371
+	// let km = miles × 1.609344
 }
-
 
 // Event Listeners
 convTempBtn.addEventListener("click", convertTemp);
-changeTempBtn.addEventListener("click", swapTempValues);
-// resetTempBtn.addEventListener("click", restetValues);
+// changeTempBtn.addEventListener("click", swapTempValues);
 
+//
 
-// 
-
-let btns = document.querySelectorAll('button')
-for(bt of btns){
-    bt.addEventListener('click',function checkBtn(e){
-        console.log(bt, e.target)
-        if(e.target.innerText === 'RESET'){
-            reset(e)
-        }
-    })
+let btns = document.querySelectorAll("button");
+for (bt of btns) {
+	bt.addEventListener("click", function checkBtn(e) {
+		if (e.target.innerText === "RESET") {
+			reset(e);
+		} else if (e.target.innerText === "CHANGE SIDES") {
+			swapSide(e);
+		} else {
+            convert(e)
+		}
+	});
 }
 
-function reset(e){
-    let curParent = e.target.parentElement.parentElement
-    let inputClear = curParent.querySelector('.input').value = ""
-    let result = document.querySelector('p')
-    result.textContent = "Result:"
-    result.style.color = "var(--font-color)";
-    // Change values of conversion to default
-    let conLeft = curParent.querySelector('.convert-from')
-    let conRight = curParent.querySelector('.convert-to')
-    if(e.target.classList.contains('reset-temp')){
-        conLeft.textContent = "°C"
-        conRight.textContent = "°F"
-    }else if(e.target.classList.contains('reset-length')){
-        conLeft.textContent = 'Meter'
-        conRight.textContent = "Feet"
-    } else{
-        conLeft.textContent = 'KM/h'
-        conRight.textContent = "M/h"
-    }
+// CHANGING UNIT SIDES FOR CONVERSION
+
+function swapSide(e) {
+	console.log(e);
+	let curParent = e.target.parentElement.parentElement;
+	let inputClear = curParent.querySelector(".input").value = "";
+	let result = document.querySelector("p");
+	result.textContent = "Result:";
+	result.style.color = "var(--font-color)";
+	// Change values of conversion to default
+	let conLeft = curParent.querySelector(".convert-from");
+	let conRight = curParent.querySelector(".convert-to");
+	if (e.target.classList.contains("temp-change")) {
+		if (conLeft.textContent === "°C") {
+			conLeft.textContent = "°F";
+			conRight.textContent = "°C";
+		} else {
+			conLeft.textContent = "°C";
+			conRight.textContent = "°F";
+		}
+	} else if (e.target.classList.contains("length-change")) {
+		if (conLeft.textContent === "Meter") {
+			conLeft.textContent = "Feet";
+			conRight.textContent = "Meter";
+		} else {
+			conLeft.textContent = "Meter";
+			conRight.textContent = "Feet";
+		}
+	} else {
+		if (conLeft.textContent === "KM/h") {
+			conLeft.textContent = "M/h";
+			conRight.textContent = "KM/h";
+		} else {
+			conLeft.textContent = "KM/h";
+			conRight.textContent = "M/h";
+		}
+	}
+}
+
+// RESETING ALL VALUES OF CURRENT CONVERTER TO DEFAULT
+
+function reset(e) {
+	let curParent = e.target.parentElement.parentElement;
+	let inputClear = (curParent.querySelector(".input").value = "");
+	let result = document.querySelector("p");
+	result.textContent = "Result:";
+	result.style.color = "var(--font-color)";
+	// Change values of conversion to default
+	let conLeft = curParent.querySelector(".convert-from");
+	let conRight = curParent.querySelector(".convert-to");
+	if (e.target.classList.contains("reset-temp")) {
+		conLeft.textContent = "°C";
+		conRight.textContent = "°F";
+	} else if (e.target.classList.contains("reset-length")) {
+		conLeft.textContent = "Meter";
+		conRight.textContent = "Feet";
+	} else {
+		conLeft.textContent = "KM/h";
+		conRight.textContent = "M/h";
+	}
 }
