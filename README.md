6f710426ccf949928a06b6597d77be891b69f982
Michal Krawczyk <michalkrawczyk1@gmail.com>
2022-08-31 16:17:20 +0100

    html structure and JS functionality added for modal page with HOW TO USE

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Project-02-Code-Institute
 3 Most popular values converter
+
+modal idea https://www.youtube.com/watch?v=XH5OW46yO8I
--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,19 @@ document
 	.querySelector(".date")
 	.appendChild(document.createTextNode(new Date().getFullYear()));
 
+// Modal window ---how to use--- 
+
+const modalOpen = document.querySelector('.modal-open')
+const modalClose = document.querySelector('.modal-close')
+const modal = document.querySelector('.modal-wrapper')
+console.log(modalOpen, modalClose, modal)
+modalOpen.addEventListener('click', ()=> {
+	modal.classList.add('show')
+})
+modalClose.addEventListener('click', ()=> {
+	modal.classList.remove('show')
+})
+
 // Add event listener to each button with correct function
 
 let btns = document.querySelectorAll("button");
